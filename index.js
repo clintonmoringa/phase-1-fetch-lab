8b3981dbddb8ca5b57e039844e9a6b94051e6403
Clinton <+>
2023-07-31 16:00:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+
+
+function renderBooks(books) {
   fetch("https://anapioficeandfire.com/api/books")
   .then((resp) => resp.json())
   .then((json) => console.log(json));
-  
-}
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
+const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
+
+function initilize(){
+  renderBooks()
+} initilize()
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
